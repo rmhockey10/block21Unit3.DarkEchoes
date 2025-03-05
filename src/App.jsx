85a8765318f76a635963ee54d5278c552d26af44
rmhockey10 <rees.m.hannah@gmail.com>
2025-03-05 13:37:46 -0500

    uploading final jsx and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,54 @@
+import { useState } from "react";
+import { episodeList } from "./data";
+
 export default function App() {
   // TODO
+  const [episodes] = useState(episodeList);
+  const [selectedEpisode, setSelectedEpisode] = useState();
+
+  function EpisodeList() {
+    return (
+      <section className="episodeList">
+        <h2>Episode List</h2>
+        <ul className="episodeList">
+          {episodes.map((episode) => (
+            <li key={episode.id} onClick={() => setSelectedEpisode(episode)}>
+              {episode.title}
+            </li>
+          ))}
+        </ul>
+      </section>
+    );
+  }
+
+  function EpisodeDetails() {
+    if (!selectedEpisode) {
+      return (
+        <section className="details">
+          <h2>Episode Details</h2>
+          <p>Select an episode to learn more.</p>
+        </section>
+      );
+    }
+
+    return (
+      <section className="details">
+        <h2>{selectedEpisode.title}</h2>
+        <p>Episode: {selectedEpisode.id}</p>
+        <p>Description: {selectedEpisode.description}.</p>
+      </section>
+    );
+  }
+
+  return (
+    <>
+      <header>
+        <h1>Dark Echoes</h1>
+      </header>
+      <main>
+        <EpisodeList />
+        <EpisodeDetails />
+      </main>
+    </>
+  );
 }
